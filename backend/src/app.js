35daf065d3acbabf809fe6249b6402bfe1a476c8
Anugra Gupta <anugragupta07@gmail.com>
2025-07-22 03:14:08 +0530

    fix

--- backend/src/app.js
+++ backend/src/app.js
@@ -10,7 +10,7 @@ const app = express();
 app.disable('etag');
 
 app.use(cors({
-  origin: ['http://localhost:3000', 'http://localhost:3001'],
+  origin: ['http://localhost:3000', 'http://localhost:3001' , 'https://capstone-sem2-ds.vercel.app'],
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization']
